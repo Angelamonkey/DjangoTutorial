55e34d91b24adcf0afd0e4e28ee0ffbda9e70312
monkey <m18859608981@163.com>
2025-12-10 15:45:29 +0800

    提交 DjangoTutorial part4 代码

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,33 +1,49 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.template import loader
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from polls.models import QuestionModel
-
-
-def index(request):
-    latest_question_list = QuestionModel.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    # template = loader.get_template("polls/index.html")
-    # return HttpResponse(template.render(context, request))
-    return render(request, "polls/index.html", context)
+from .models import QuestionModel, ChoiceModel
 
 
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-    # try:
-    #     question = QuestionModel.objects.get(pk=question_id)
-    # except QuestionModel.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(QuestionModel, pk=question_id)
-    # question = get_list_or_404(QuestionModel)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return QuestionModel.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = QuestionModel
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = QuestionModel
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(QuestionModel, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, ChoiceModel.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
