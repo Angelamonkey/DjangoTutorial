0bf9802e847bf0fca2a6f39a9fc0caf8e9643b13
monkey <m18859608981@163.com>
2025-12-10 15:36:46 +0800

    提交 DjangoTutorial part3 代码

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,5 +1,33 @@
-from django.http import HttpResponse
-from datetime import datetime
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.template import loader
+
+from polls.models import QuestionModel
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = QuestionModel.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = QuestionModel.objects.get(pk=question_id)
+    # except QuestionModel.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(QuestionModel, pk=question_id)
+    # question = get_list_or_404(QuestionModel)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
